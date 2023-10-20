c084b888203d49b23ed7741106300aa4848bfbe3
shaikEhtesham <shaiksham3556@gmail.com>
2023-10-20 21:59:49 +0530

    created sign up page and connected to mongodb

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const usermodel = require('./models/user');
+const user = require('./models/user');
 const app = express();
-const PORT = 8001;
+const PORT = 8002;
 
 // middlewares
 app.set('view engine','ejs');
 app.set('views','./views')
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// MongoDB connection
+
+mongoose.connect('mongodb://127.0.0.1:27017/necnappdb').then(()=>{
+    console.log("Connected to MongoDB");
+}).catch(err=> console.error("Error connecting to MongoDB",err));
 
 
 // routes
@@ -16,6 +26,34 @@ app.get('/',(req,res)=>{
 })
 app.get('/home',(req,res)=>{
     res.send("hello ")
+})
+app.get('/signup',(req,res)=>{
+    res.render('form');
+})
+app.post('/signup',async (req,res)=>{
+    const {username,rollnumber,email,password} = req.body;
+    try{
+    const newuser = await usermodel.create({
+        username,
+        rollnumber,
+        email,
+        password,
+    });
+        res.send("user created sucessfully");}
+        catch(err){
+    console.error("error createing user",err);
+}
+
+});
+
+app.get('/display',async (req,res)=>{
+    try {
+        const users = await usermodel.find({}); 
+        res.render('display',{users})
+    } catch (error) {
+        console.error('Error fetching users:', error);
+        res.status(500).send('Error fetching users');
+    }
 })
 
 
